8ef9b2f251af33812a8233b3d6600b612b46b9bc
RenanChagas <renanchagasw@gmail.com>
2015-10-21 11:15:39 -0200

    Criação do baseline
    1.0.0.

---
+++ src/main/java/com/websystique/springsecurity/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.websystique.springsecurity.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.websystique.springsecurity.dao.UserDaoImpl;
+import com.websystique.springsecurity.model.User;
+
+@Service("UserServiceImpl")
+@Transactional
+public class UserServiceImpl {
+
+	@Autowired
+	private UserDaoImpl dao;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	
+	public void save(User user){
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		dao.save(user);
+	}
+	
+	public User findById(int id) {
+		return dao.findById(id);
+	}
+
+	public User findBySso(String sso) {
+		return dao.findBySSO(sso);
+	}
+	
+}
